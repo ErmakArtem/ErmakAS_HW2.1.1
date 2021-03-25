17f703bd327a72b59835c0bab7fe218432f305ad
Artem <artem-ermak-696@yande.by>
2021-03-25 20:05:32 +0300

    Initial Commit

---
+++ ErmakAS_HW2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErmakAS_HW2.1.1
+//
+//  Created by Артем Ермак on 3/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
